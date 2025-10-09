9a3a9775da644aabd56a06e8b5b1c1959fd82141
MuhiuddinHomeDesktop <muhiuddinanik98@gmail.com>
2025-10-10 03:06:16 +0600

    Finalize configuration for Render deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['https://muhiuddins-portfolio.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['muhiuddins-portfolio.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition

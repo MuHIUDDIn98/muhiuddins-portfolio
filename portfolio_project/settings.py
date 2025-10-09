bcd963931f63b8ae9e6266b580fe3f1aacfa2476
MuhiuddinHomeDesktop <muhiuddinanik98@gmail.com>
2025-10-10 03:17:46 +0600

    FIX: Configure Cloudinary settings

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "portfolio",
+    'cloudinary_storage', # ADD THIS
+    'cloudinary',         # ADD THIS
 ]
 
 MIDDLEWARE = [
